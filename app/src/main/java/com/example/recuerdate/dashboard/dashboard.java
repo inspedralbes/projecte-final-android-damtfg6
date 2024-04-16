6e9f32da34f7f0810010b038deef8e62d687a5a6
C0ural1 <albertcoralm@gmail.com>
2024-04-16 13:21:04 +0200

    Canvis dashboard

---
+++ app/src/main/java/com/example/recuerdate/dashboard/dashboard.java
@@ -0,0 +1,15 @@
+package com.example.recuerdate.dashboard;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.recuerdate.R;
+public class dashboard extends Fragment {
+
+
+}
