4769182cbfd63510e6cd36f63678688d6076645d
C0ural1 <albertcoralm@gmail.com>
2024-04-17 23:02:06 +0200

    commit

--- app/src/main/java/com/example/recuerdate/dashboard/dashboard.java
+++ app/src/main/java/com/example/recuerdate/dashboard/dashboard.java
@@ -7,9 +7,21 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import com.example.recuerdate.R;
 public class dashboard extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_dashboard, container, false);
 
 
+        return rootView;
+    }
 }
