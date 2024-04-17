6f3946621d619f2c0a2cc5fb6a7cbe28c07eb41b
C0ural1 <albertcoralm@gmail.com>
2024-04-17 13:20:36 +0200

    Canvis familiars

---
+++ app/src/main/java/com/example/recuerdate/FamiliarsActivity.java
@@ -0,0 +1,13 @@
+package com.example.recuerdate;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FamiliarsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_familiars);
+    }
+}
