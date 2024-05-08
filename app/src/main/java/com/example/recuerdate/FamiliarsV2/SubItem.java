35bfb0cd9665b81a3b4704fcda19c629095e7f62
PanDyyG <a22angcamrui@inspedralbes.cat>
2024-05-08 12:14:21 +0200

    familiarsV2 to server

--- app/src/main/java/com/example/recuerdate/FamiliarsV2/SubItem.java
+++ app/src/main/java/com/example/recuerdate/FamiliarsV2/SubItem.java
@@ -1,24 +1,38 @@
 package com.example.recuerdate.FamiliarsV2;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.util.Base64;
 import android.widget.ImageView;
 
+import java.io.ByteArrayOutputStream;
+
 public class SubItem {
-    private ImageView subItemImage;
+    private String subItemImageBase64; // Guarda la imagen como una cadena Base64
     private String subItemTitle;
     private String subItemDesc;
 
-    public SubItem(String subItemTitle, String subItemDesc, ImageView subItemImage) {
+    public SubItem(String subItemTitle, String subItemDesc, ImageView subItemImage, Context context) {
         this.subItemTitle = subItemTitle;
         this.subItemDesc = subItemDesc;
-        this.subItemImage = subItemImage;
+        this.subItemImageBase64 = imageViewToBase64(subItemImage, context);
     }
 
-    public ImageView getSubItemImage() {
-        return subItemImage;
+    // Convertir ImageView a Base64
+    private String imageViewToBase64(ImageView imageView, Context context) {
+        Bitmap bitmap = ((BitmapDrawable) imageView.getDrawable()).getBitmap();
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
+        byte[] byteArray = byteArrayOutputStream.toByteArray();
+        return Base64.encodeToString(byteArray, Base64.DEFAULT);
     }
 
-    public void setSubItemImage(ImageView subItemImage) {
-        this.subItemImage = subItemImage;
+    // Convertir Base64 a Bitmap
+    public Bitmap getSubItemImage(Context context) {
+        byte[] decodedString = Base64.decode(subItemImageBase64, Base64.DEFAULT);
+        return BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
     }
 
     public String getSubItemTitle() {
@@ -37,3 +51,4 @@ public class SubItem {
         this.subItemDesc = subItemDesc;
     }
 }
+
