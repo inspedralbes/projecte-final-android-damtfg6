2341d9a5f859ba895d40ef5b25eedc78682a0f1c
PanDyyG <a22angcamrui@inspedralbes.cat>
2024-05-23 09:59:21 +0200

    arreglo

--- app/src/main/java/com/example/recuerdate/Settings.java
+++ app/src/main/java/com/example/recuerdate/Settings.java
@@ -1,6 +1,6 @@
 package com.example.recuerdate;
 
 public class Settings {
-    public static final String SERVER = "http://192.168.18.11";
+    public static final String SERVER = "http://recuerdate.dam.inspedralbes.cat";
     public static final int PORT = 3672;
 }
